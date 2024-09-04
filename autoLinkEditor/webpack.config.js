cbc4031587e3742cb0a7c95f1ae0107f8d9e5dbd
Ryan Xu <cxu@example.com>
2024-09-04 22:36:31 +1200

    update package.json for autoLinkEditor

--- autoLinkEditor/webpack.config.js
+++ autoLinkEditor/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './autoLinkEditor/src/index.js',
+  entry: './src/index.js',
   output: {
     filename: 'autoLinkEditor.bundle.js',
     path: path.resolve(__dirname, '../dist'),
