81e32fba90e64bc01ca058c01ba0ca2d70c80be2
Ryan Xu <cxu@example.com>
2024-10-02 21:25:54 +1300

    Add dist as release folder

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'autolink-editor.bundle.js',
+    filename: 'autolink-editor.min.js',
     path: path.resolve(__dirname, './dist'),
   },
   module: {
